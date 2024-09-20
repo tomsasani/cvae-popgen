a420f556f09a2c921e1c84fa4f386b00d466050d
Tom Sasani <tomsasani1493@gmail.com>
2024-09-20 15:08:23 -0600

    don't apply batchnorm or relu to final convolution operation in decoder

--- models.py
+++ models.py
@@ -5,6 +5,74 @@ import torchvision
 import torchvision.transforms.functional as F
 
 
+class Basic1DCNN(nn.Module):
+    def __init__(self, in_W: int = 32, intermediate_dim: int = 128, latent_dim: int = 3):
+        super(Basic1DCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(1, 5), padding="same")
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(1, 5), padding="same")
+        self.pool = nn.MaxPool2d(kernel_size=(1, 2), stride=(1, 2))
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(64 * (in_W // 4), intermediate_dim)
+        self.fc2 = nn.Linear(intermediate_dim, latent_dim)
+
+    def forward(self, x):
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+
+        x = torch.amax(x, dim=2)
+
+        x = self.flatten(x)
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc2(x)
+
+        return x
+
+class FinetuneResnet(nn.Module):
+
+    def __init__(
+        self,
+        pretrained_model,
+        representation_dims: int = 512,
+        latent_dims: int = 3,
+    ):
+        super(FinetuneResnet, self).__init__()
+
+        pretrained_model.fc = nn.Identity()
+
+        self.representation = pretrained_model
+        self.latent = nn.Sequential(
+                nn.Linear(representation_dims, representation_dims),
+                nn.ReLU(),
+                nn.Dropout(0.2),
+                nn.Linear(representation_dims, latent_dims),
+            )
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        # produce output of final conv layer/avg pool
+        representation = self.representation(x)
+
+        # relu non-linearity in encoder
+        representation = self.relu(representation)
+        
+        latent = self.latent(representation)        
+        latent = self.relu(latent)
+        
+        return latent
+
 class ConvBlock2D(nn.Module):
 
     def __init__(
@@ -13,8 +81,10 @@ class ConvBlock2D(nn.Module):
         in_channels: int,
         out_channels: int,
         kernel_size: Union[int, Tuple[int]],
-        stride: int,
-        padding: int,
+        stride: Union[int, Tuple[int]],
+        padding: Union[int, Tuple[int]],
+        batch_norm: bool = True,
+        activation: bool = True,
     ):
         super(ConvBlock2D, self).__init__()
 
@@ -24,11 +94,16 @@ class ConvBlock2D(nn.Module):
             kernel_size=kernel_size,
             stride=stride,
             padding=padding,
+            bias=False,
         )
 
-        relu = nn.ReLU()
+        relu = nn.LeakyReLU(0.2)
         norm = nn.BatchNorm2d(out_channels)
-        layers = [conv, norm, relu]
+        layers = [conv]
+        if activation:
+            layers.append(relu)
+        if batch_norm:
+            layers.append(norm)
         self.block = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -43,9 +118,11 @@ class ConvTransposeBlock2D(nn.Module):
         in_channels: int,
         out_channels: int,
         kernel_size: Union[int, Tuple[int]],
-        stride: int,
-        padding: int,
-        output_padding: int,
+        stride: Union[int, Tuple[int]],
+        padding: Union[int, Tuple[int]],
+        output_padding: Union[int, Tuple[int]],
+        batch_norm: bool = True,
+        activation: bool = True,
     ):
         super(ConvTransposeBlock2D, self).__init__()
 
@@ -56,16 +133,21 @@ class ConvTransposeBlock2D(nn.Module):
             stride=stride,
             padding=padding,
             output_padding=output_padding,
+            bias=False,
         )
 
-        relu = nn.ReLU()
+        relu = nn.LeakyReLU(0.2)
         norm = nn.BatchNorm2d(out_channels)
-        #layers = [conv, norm, relu]
-        layers = [conv, relu]
+        layers = [conv]
+        if activation:
+            layers.append(relu)
+        if batch_norm:
+            layers.append(norm)
         self.block = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.block(x)
+
 
 class CNN(nn.Module):
     def __init__(
@@ -130,11 +212,11 @@ class CNN(nn.Module):
 
         x = self.fc_intermediate(x)
         x = self.relu(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
 
         x = self.fc1(x)
         x = self.relu(x)
-        x = self.dropout(x)
+        # x = self.dropout(x)
         x = self.fc2(x)
 
         return x
@@ -143,14 +225,15 @@ class CNN(nn.Module):
 class Encoder(nn.Module):
     def __init__(
         self,
+        *,
         in_channels: int,
         latent_dim: int,
-        kernel_size: int = 5,
-        stride: int = 2,
-        padding: int = 1,
+        kernel_size: Union[int, Tuple[int]] = 5,
+        stride: Union[int, Tuple[int]] = 2,
+        padding: Union[int, Tuple[int]] = 1,
         hidden_dims: List[int] = None,
         intermediate_dim: int = 128,
-        in_H: int = 32,
+        in_HW: Tuple[int] = (32, 32),
     ) -> None:
         super(Encoder, self).__init__()
 
@@ -160,7 +243,17 @@ class Encoder(nn.Module):
             hidden_dims = [16, 32, 64, 128, 256]
 
         # figure out final size of image after convs
-        out_H = int(in_H / (2 ** len(hidden_dims)))
+        out_H, out_W = in_HW
+        out_W //= 2 ** len(hidden_dims)
+
+        # image height will only decrease if we're using square filters
+        if type(kernel_size) is int or (
+            len(kernel_size) > 1
+            and kernel_size[0] == kernel_size[1]
+            and kernel_size[0] > 1
+        ):
+            out_H //= 2 ** len(hidden_dims)
+
 
         encoder_blocks = []
         for h_dim in hidden_dims:
@@ -171,6 +264,8 @@ class Encoder(nn.Module):
                 kernel_size=kernel_size,
                 stride=stride,
                 padding=padding,
+                batch_norm=True,
+                activation=True,
             )
             encoder_blocks.append(block)
 
@@ -179,10 +274,9 @@ class Encoder(nn.Module):
         self.encoder_conv = nn.Sequential(*encoder_blocks)
 
         self.fc_intermediate = nn.Linear(
-            hidden_dims[-1] * out_H * out_H,
+            hidden_dims[-1] * out_H * out_W,
             intermediate_dim,
         )
-        self.relu = nn.ReLU()
 
         self.fc_mu = nn.Linear(
             intermediate_dim,
@@ -192,6 +286,9 @@ class Encoder(nn.Module):
             intermediate_dim,
             latent_dim,
         )
+
+        self.relu = nn.LeakyReLU(0.2)
+
 
     def forward(self, x):
         x = self.encoder_conv(x)
@@ -215,24 +312,36 @@ class Decoder(nn.Module):
         self,
         out_channels: int,
         latent_dim: int,
-        kernel_size: int = 5,
-        stride: int = 2,
-        padding: int = 1,
-        output_padding: int = 1,
+        kernel_size: Union[int, Tuple[int]] = (5, 5),
+        stride: Union[int, Tuple[int]] = 2,
+        padding: Union[int, Tuple[int]] = 1,
+        output_padding: Union[int, Tuple[int]] = 1,
         hidden_dims: List[int] = None,
         intermediate_dim: int = 128,
-        in_H: int = 32,
+        in_HW: Tuple[int] = (32, 32),
     ) -> None:
         super(Decoder, self).__init__()
 
         if hidden_dims is None:
             hidden_dims = [16, 32, 64, 128, 256]
 
+        # figure out final dimension to which we 
+        # need to reshape our filters before decoding
         self.final_dim = hidden_dims[-1]
 
         # figure out final size of image after convs
-        out_H = int(in_H / (2 ** len(hidden_dims)))
+        out_H, out_W = in_HW
+        out_W //= 2 ** len(hidden_dims)
+        # image height will only decrease if we're using square filters
+        if type(kernel_size) is int or (
+            len(kernel_size) > 1
+            and kernel_size[0] == kernel_size[1]
+            and kernel_size[0] > 1
+        ):
+            out_H //= 2 ** len(hidden_dims)
+
         self.out_H = out_H
+        self.out_W = out_W
 
         self.decoder_input = nn.Linear(
             latent_dim,
@@ -241,7 +350,7 @@ class Decoder(nn.Module):
 
         self.decoder_upsize = nn.Linear(
             intermediate_dim,
-            hidden_dims[-1] * out_H * out_H,
+            hidden_dims[-1] * out_H * out_W,
         )
 
         decoder_blocks = []
@@ -250,7 +359,6 @@ class Decoder(nn.Module):
         hidden_dims.reverse()
 
         for i in range(len(hidden_dims) - 1):
-
             block = ConvTransposeBlock2D(
                 in_channels=hidden_dims[i],
                 out_channels=hidden_dims[i + 1],
@@ -258,26 +366,25 @@ class Decoder(nn.Module):
                 stride=stride,
                 padding=padding,
                 output_padding=output_padding,
+                batch_norm=True,
+                activation=True,
             )
             decoder_blocks.append(block)
 
         self.decoder_conv = nn.Sequential(*decoder_blocks)
-        self.relu = nn.ReLU()
+        self.relu = nn.LeakyReLU(0.2)
 
+        # NOTE: no batch norm and no ReLu in final block
         final_block = [
             ConvTransposeBlock2D(
                 in_channels=hidden_dims[-1],
-                out_channels=hidden_dims[-1],
+                out_channels=out_channels,
                 kernel_size=kernel_size,
                 stride=stride,
                 padding=padding,
                 output_padding=output_padding,
-            ),
-            nn.Conv2d(
-                hidden_dims[-1],
-                out_channels,
-                kernel_size=kernel_size,
-                padding=padding,
+                batch_norm=False,
+                activation=False,
             ),
             nn.Sigmoid(),
         ]
@@ -292,7 +399,7 @@ class Decoder(nn.Module):
         x = self.relu(x)
         # reshape
 
-        x = x.view((-1, self.final_dim, self.out_H, self.out_H))
+        x = x.view((-1, self.final_dim, self.out_H, self.out_W))
         x = self.decoder_conv(x)
 
         x = self.final_block(x)
@@ -374,7 +481,6 @@ class CVAE(nn.Module):
         s_encoder,
         z_encoder,
         decoder,
-        latent_dim: int = 2,
         disentangle: bool = False,
     ):
         super(CVAE, self).__init__()
@@ -408,30 +514,45 @@ class CVAE(nn.Module):
         tg_s_mu, tg_s_log_var = self.qs(tg_inputs)
         tg_s = self.reparameterize(tg_s_mu, tg_s_log_var)
 
-        # step 2: pass background features through just
-        # the irrelevant encoder
+        # step 2: pass background features through
+        # the irrelevant and salient encoders
 
         bg_z_mu, bg_z_log_var = self.qz(bg_inputs)
         bg_z = self.reparameterize(bg_z_mu, bg_z_log_var)
+
+        bg_s_mu, bg_s_log_var = self.qs(bg_inputs)
+        bg_s = self.reparameterize(bg_s_mu, bg_s_log_var)
 
         # step 3: decode
 
         # decode the target outputs using both the salient and
         # irrelevant features
-        tg_outputs = self.decoder(torch.cat([tg_s, tg_z], dim=-1))
+        tg_outputs = self.decoder(torch.cat([tg_s, tg_z], dim=1))
         # we decode the background outputs using only the irrelevant
         # features
-        zeros = torch.zeros_like(bg_z)
-        bg_outputs = self.decoder(torch.cat([zeros, bg_z], dim=-1))
+        bg_outputs = self.decoder(torch.cat([torch.zeros_like(bg_s), bg_z], dim=1))
+
+        fg_outputs = self.decoder(torch.cat([tg_s, torch.zeros_like(tg_z)], dim=1))
+
 
         out_dict = {
             "tg_out": tg_outputs,
             "bg_out": bg_outputs,
+            "fg_out": fg_outputs,
+
             "tg_s_mu": tg_s_mu,
             "tg_s_log_var": tg_s_log_var,
             "tg_s": tg_s,
+
             "tg_z_mu": tg_z_mu,
             "tg_z_log_var": tg_z_log_var,
+            "tg_z": tg_z,
+
+            "bg_s": bg_s,
+            "bg_s_mu": bg_s_mu,
+            "bg_s_log_var": bg_s_log_var,
+
+            "bg_z": bg_z,
             "bg_z_mu": bg_z_mu,
             "bg_z_log_var": bg_z_log_var,
         }
@@ -441,31 +562,82 @@ class CVAE(nn.Module):
 
 if __name__ == "__main__":
 
-    encoder = Encoder(
-        in_channels=1,
-        latent_dim=2,
-        kernel_size=3,
-        stride=2,
-        padding=1,
-        intermediate_dim=128,
-        in_H=64,
+    KERNEL_SIZE = (5, 5) #(1, 5)
+    STRIDE = (2, 2) #(1, 2)
+    PADDING = (2, 2) #(0, 2)
+    OUTPUT_PADDING = (1, 1) #(0, 1)
+    INTERMEDIATE_DIM = 64
+    IN_HW = (64, 64)# (100, 32)
+    HIDDEN_DIMS = [32, 64, 128, 256]
+    LATENT_S = 2
+    LATENT_Z = 4
+
+    s_encoder = Encoder(
+        in_channels=3,
+        latent_dim=LATENT_S,
+        kernel_size=KERNEL_SIZE,
+        stride=STRIDE,
+        padding=PADDING,
+        intermediate_dim=INTERMEDIATE_DIM,
+        in_HW=IN_HW,
+        hidden_dims=HIDDEN_DIMS,
+    )
+
+    z_encoder = Encoder(
+        in_channels=3,
+        latent_dim=LATENT_Z,
+        kernel_size=KERNEL_SIZE,
+        stride=STRIDE,
+        padding=PADDING,
+        intermediate_dim=INTERMEDIATE_DIM,
+        in_HW=IN_HW,
+        hidden_dims=HIDDEN_DIMS,
     )
 
     decoder = Decoder(
-        latent_dim=2,
-        kernel_size=3,
-        stride=2,
-        padding=1,
-        output_padding=1,
-        intermediate_dim=128,
-        out_channels=1,
-        in_H=64,
+        out_channels=3,
+        latent_dim=LATENT_S,
+        kernel_size=KERNEL_SIZE,
+        stride=STRIDE,
+        padding=PADDING,
+        intermediate_dim=INTERMEDIATE_DIM,
+        output_padding=OUTPUT_PADDING,
+        in_HW=IN_HW,
+        hidden_dims=HIDDEN_DIMS,
     )
 
-    model = VAE(encoder=encoder, decoder=decoder,)
-    model = model.to("cuda")
+    model = VAE(encoder=s_encoder, decoder=decoder,)
+    model = model.to("cpu")
 
-    test = torch.rand(size=(100, 1, 64, 64)).to("cuda")
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print (pytorch_total_params)
+
+    test = torch.rand(size=(100, 3, 64, 64)).to("cpu")
 
     out, mu, log_var, z = model(test)
     print (out.shape)
+
+    decoder = Decoder(
+        out_channels=3,
+        latent_dim=LATENT_S + LATENT_Z,
+        kernel_size=KERNEL_SIZE,
+        stride=STRIDE,
+        padding=PADDING,
+        intermediate_dim=INTERMEDIATE_DIM,
+        output_padding=OUTPUT_PADDING,
+        in_HW=IN_HW,
+        hidden_dims=HIDDEN_DIMS,
+    )
+    print (decoder.final_dim)
+
+    model = CVAE(s_encoder=s_encoder, z_encoder=z_encoder, decoder=decoder,)
+    model = model.to("cpu")
+
+    pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print (pytorch_total_params)
+
+    x = torch.rand(size=(100, 3, 64, 64)).to("cpu")
+    x_ = torch.rand(size=(100, 3, 64, 64)).to("cpu")
+
+    cvae_dict = model(x, x_)
+    print ([(k, v.shape) for k,v in cvae_dict.items()])
